c18c5f9e6eac992e80b92a0d7ff1080a37597596
jomerary <merary2004garcia@gmail.com>
2025-05-02 21:36:34 -0600

    tesst

--- parcial-test-1/Program.cs
+++ parcial-test-1/Program.cs
@@ -11,6 +11,7 @@ namespace parcial_test_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
 
         }
     }
